8bb399d0cddbce5c58cc4fc38aac4331fd36f899
ChusAlgar <chus.algardiaz@gmail.com>
2022-03-16 17:49:05 +0100

    Refactoring MASK code (kmeans_tree_npdist_modulado.py) with different metrics Test cod: main_test_dis_modulado.py

---
+++ mask/experiments/test_indexing/main_test_dist_modulado.py
@@ -0,0 +1,54 @@
+import logging
+import mask.kmeans_tree_nparray as kt
+import mask.kmeans_tree_npdist_modulado as ktd
+import mask.data_test as dt
+
+
+
+logging.basicConfig(filename='../../../logs/modulado/chebyshev/result_km_overlap5.log', filemode='w', format='%(asctime)s - %(name)s - %(message)s',
+                    level=logging.INFO)
+
+# Parámetros de entrada comunes a todas las simulaciones:
+nclouds = 8
+tam_grupo = 16
+n_centroides = 8
+npc = 200
+overlap = True
+
+
+# SubText 1_kt
+logging.info('------------------------------------------------------------------------')
+logging.info(' ')
+logging.info('Sub Text MASK, kmeans_tree_nparray')
+kt.kmeans_tree(nclouds,npc,tam_grupo,n_centroides,overlap)
+logging.info(' ')
+
+
+# SubText 2_MASKmodulado
+logging.info('------------------------------------------------------------------------')
+logging.info(' ')
+logging.info('Sub Text MASK modulado, kmeans_tree_npdist_modulado')
+
+# 1º Generamos los datos
+vector_original, coordx, coordy, puntos_nube = dt.generate_data_gaussian_clouds(nclouds, npc, overlap)
+
+# 2º Construimos el árbol
+metrica = 'chebyshev'
+cant_ptos = nclouds * npc
+n_capas, grupos_capa, puntos_capa, labels_capa = ktd.kmeans_tree(cant_ptos, tam_grupo,
+                                                                 n_centroides, metrica, vector_original)
+
+# Pinto las nubes de puntos originales junto con los centroides sacados
+dt.pinta(coordx, coordy, puntos_capa[n_capas], npc, nclouds)
+# dt.pinta_info_nube(coordx, coordy, puntos_capa, grupos_capa, labels_capa)
+
+# 3º Hacemos la búsqueda
+aciertos, fallos = ktd.kmeans_search(n_capas, n_centroides, vector_original, puntos_nube, metrica,
+                                     grupos_capa, puntos_capa, labels_capa)
+
+print("Porcentaje de aciertos en la iteración ", iter, ": ", aciertos * 100 / (npc * nclouds))
+print("Porcentaje de fallos en la iteración ", iter, ": ", fallos * 100 / (npc * nclouds))
+logging.info('Porcentaje de aciertos= %s', aciertos * 100 / (npc * nclouds))
+logging.info('Porcentaje de fallos= %s', fallos * 100 / (npc * nclouds))
+
+logging.info(' ')
