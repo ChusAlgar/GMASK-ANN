c4b25ba200d1cab370fa253370800d04e7647762
elenagarciamorato <e.garciapi@alumnos.urjc.es>
2022-04-19 20:36:00 +0200

    Experiment to test FLANN over image dataset (MNist) improved. Factorized code to use FLANN over different datasets. Performance evaluation implemented (hit = the nn returned is the same returned by linear exploration, fail = the nn returned is NOT the same returned by linear exploration)

--- other_algorithms/main_test_FLANN.py
+++ other_algorithms/main_test_FLANN.py
@@ -4,7 +4,7 @@ from numpy.random import *
 import random
 import pandas as pd
 from sklearn import preprocessing
-from FLANN_npdist_modulado import FLANN_tree, FLANN_nn
+from FLANN_npdist_modulado import FLANN_tree, FLANN_nn, benchmark
 
 
 # Set constants for experiments
@@ -12,11 +12,11 @@ nclouds = 8
 npc = 200
 ncentroids = 128  # At MASK, ncentroids = tam_grupo*n_centroides = 8*16 = 128
 normaliza = False
-algorithm = 'kmeans'  # Possible values: linear, kdtree, kmeans, composite, saved, autotuned
-distance_type = 'euclidean'  # Possible values: euclidean, manhattan, minkowski, max_dist, hik, hellinger, cs, kl.
-knn = 2  # Number of Nearest Neighbourgs to be found
+algorithm = 'kmeans'  # Possible values: linear, kdtree, kmeans, composite, autotuned - default: kdtree
+distance_type = 'euclidean'  # Possible values: euclidean, manhattan, minkowski, max_dist, hik, hellinger, cs, kl. - default:
+knn = 5  # Number of Nearest Neighbourgs to be found
 
-
+'''
 ####### Test FLANN with Image Dataset (MNist) ########
 
 # DATASET LOADING AND PREPROCESSING
@@ -38,21 +38,22 @@ for i in range(len(images)):
 images = images.drop(columns='label').to_numpy()
 
 # For this experiment, compose the dataset and the query
-#seq_buscada = images[0:30]
 #images = images[20:]
 
-# Generate a testset of n elements contained in the original dataset
-seq_buscada = np.array(random.sample(images.tolist(), 1000))
-
-# print(seq_buscada)
+seq_buscada = images[0:30]  # Generate a testset with the n first elements contained in the original dataset
+#seq_buscada = np.array(random.sample(images.tolist(), 1000)) # Generate a testset of n elements contained in the original dataset
 
 # CENTROIDS AND KNN
 
 # Using FLANN, build the index tree and generate the num_centroids describing the data
-centroids = FLANN_tree(images, ncentroids, normaliza, distance_type)
+centroids = FLANN_tree(images, ncentroids, distance_type, algorithm)
 
-# Using FLANN, find the knn nearest neighbors and provide number of hit/miss
-nn = FLANN_nn(images, seq_buscada, knn, distance_type, algorithm)
+# Using FLANN, find the knn nearest neighbors
+nn, dists = FLANN_nn(images, seq_buscada, knn, distance_type, algorithm)
+
+# Evaluate the performance of the algorithm
+hit, miss = benchmark(images, seq_buscada, nn, dists, distance_type)
+
 
 '''
 
@@ -60,22 +61,24 @@ nn = FLANN_nn(images, seq_buscada, knn, distance_type, algorithm)
 
 # Generate n gaussian clouds and store them into an array
 vector, coordx, coordy, puntos_nube = dt.generate_data_gaussian_clouds(nclouds, npc, overlap=True)
-vector = np.array(zip(coordx[0], coordy[0]))  # Dont use indexes to build vector!
+#vector = np.array(zip(coordx[0], coordy[0]))  # Dont use indexes to build vector
+vector = np.array(vector)
 
 # If normaliza, normalize the dataset
 if normaliza:
     vector = preprocessing.normalize(vector, axis=0, norm='l2')
-    
-# Using FLANN, build the index tree and generate the ncentroids describing the data
-centroids = FLANN_tree(vector, ncentroids, normaliza, algorithm, distance_type)
+
+# For this experiment, compose the dataset and the query
+seq_buscada = np.array(random.sample(vector, 30)) # Generate a testset of n elements contained in the original gaussian clouds
+
+# Using FLANN, build the index tree and generate the num_centroids describing the data
+centroids = FLANN_tree(vector, ncentroids, distance_type, algorithm)
+
+# Using FLANN, find the knn nearest neighbors
+nn, dists = FLANN_nn(vector, seq_buscada, knn, distance_type, algorithm)
+
+# Evaluate the performance of the algorithm
+hit, miss = benchmark(vector, seq_buscada, nn, dists, distance_type)
 
 # Draw the original gaussian clouds and the centroids computed
 dt.pinta(coordx, coordy, np.array([centroids]), npc, nclouds)
-
-# Generate a testset of n elements contained in the original gaussian clouds
-seq_buscada = np.array(random.sample(vector.tolist(), 1000))
-
-# Using FLANN, find the knn nearest neighbors and provide number of hit/miss
-nn = FLANN_nn(images, seq_buscada, knn, algorithm)
-
-'''
